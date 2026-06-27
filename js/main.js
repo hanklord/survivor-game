2e636a1a949c767b1611e7f1f24b115634b87dbf
hanklord <hanklord@users.noreply.github.com>
2026-06-28 01:53:24 +0800

    Replace melee character with Mega Man X4 Fourth Armor sprite
    - Source: spritedatabase.net X4-FourthArmor.PNG
    - Idle: 4 frames (standing pose), Run: 6 frames (dash/run)
    - Background removed, scaled to 128x128 strips

--- js/main.js
+++ js/main.js
@@ -144,8 +144,8 @@
     load('xpGem', cfg.xpGem && cfg.xpGem.image);
     load('background', cfg.background && cfg.background.image);
     // 載入近戰角色 sprite strips
-    load('melee_sprite_idle', 'assets/strips/knight_idle_10f.png');
-    load('melee_sprite_run', 'assets/strips/knight_run_10f.png');
+    load('melee_sprite_idle', 'assets/strips/x4_idle_4f.png');
+    load('melee_sprite_run', 'assets/strips/x4_run_6f.png');
     load('shield_icon', 'assets/shield_icon.png');
     // 載入各關卡背景圖
     (cfg.levels || []).forEach(function(lv, i) {
@@ -171,7 +171,7 @@
 
     // 根據角色類型設定動畫
     if (this._selectedCharacter.id === 'melee') {
-      var meleeCfg = { sprites: { idle: { file: 'assets/strips/knight_idle_10f.png', fps: 10 }, run: { file: 'assets/strips/knight_run_10f.png', fps: 10 } } };
+      var meleeCfg = { sprites: { idle: { file: 'assets/strips/x4_idle_4f.png', fps: 6 }, run: { file: 'assets/strips/x4_run_6f.png', fps: 10 } } };
       this.player.animator = this._buildAnimator('melee', meleeCfg);
       this.player.spriteDefaultRight = true;
       this._meleeAttack = new SG.MeleeAttack(this.player);
